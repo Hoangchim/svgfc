30d2e8938ef7799cebbdf34095116fcd41c913f5
Hoangchim <nguyentoanle2000@gmail.com>
2026-05-13 19:20:55 +0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -2,6 +2,7 @@ export default async () => {
   return new Response(JSON.stringify({
     project: process.env.CLIENT_ID || "missing",
     email: process.env.API_KEY || "missing",
-    key: process.env.CHECKSUM_KEY ? "exists" : "missing"
+    key: process.env.CHECKSUM_KEY || "missing",
+    Admin: key: process.env.ADMIN_IDS || "missing"
   }))
 }
