3a14d41b0b62df56959f1cda222412565b51a658
Hoangchim <nguyentoanle2000@gmail.com>
2026-05-13 19:04:46 +0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,7 +1,7 @@
 export default async () => {
   return new Response(JSON.stringify({
-    project: process.env.FB_PROJECT_ID || "missing",
-    email: process.env.FB_CLIENT_EMAIL || "missing",
-    key: process.env.FB_PRIVATE_KEY ? "exists" : "missing"
+    project: process.env.CLIENT_ID || "missing",
+    email: process.env.API_KEY || "missing",
+    key: process.env.FB_CHECKSUM_KEY ? "exists" : "missing"
   }))
 }
